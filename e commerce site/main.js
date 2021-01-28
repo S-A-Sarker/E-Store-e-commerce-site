6b4d942cf79e880f4b9cd876e3d99831569c6607
Safiul Alam Sarker <71761968+S-A-Sarker@users.noreply.github.com>
2021-01-28 22:44:58 +0600

    Add files via upload

---
+++ e commerce site/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "scrollX": true
+    } );
+} );
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
